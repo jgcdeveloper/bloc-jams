6066f44dbed0b66778382edded1e86f557870d0b
Jason Clegg <jgcdeveloper@gmail.com>
2016-09-23 19:53:51 +0500

    Possible bug... checking master

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,6 @@
-var collectionItemTemplate =
+var buildCollectionItemTemplate = function() {
+    
+    var template =
      '<div class="collection-album-container column fourth">' 
    + '   <img src="assets/images/album_covers/01.png"/>'
    + '   <div class="collection-album-info caption">'
@@ -13,17 +15,18 @@ var collectionItemTemplate =
    + '   </div>'
    + '</div>'
    ;
-
-window.onload = function() { //This will execute function upon loading of the webpage
+    //future proofing template by wrapping it in a $(), w 
+    return $(template);
     
-    /* First we need to point where we want to load our new HTML template. So, .getElementByClassName will get an array of all elements with class of album-covers. We select the first element of it using [0]. Note though at this point we only have one. We assign this to variable collectionContainer, so this spot now stores the pointer to where we want*/
-    var collectionContainer = document.getElementsByClassName('album-covers')[0]; 
+$(window).load(function() { //This will execute function upon loading of the webpage
     
-    /* This will overwrite the innerHTML stored in our first 'album-covers' class (as pointed to by collectionContainer). This makes sure we start with a blank canvas */
-    collectionContainer.innerHTML = ""; 
+    var $collectionContainer = $('.album-covers');
+    
+    $collectionContainer.empty(); 
     
     for(var i = 0; i < 12; i++){
         /* This loop will add 12 templates of our albums to the collections page using the += indicator. Note that we know collectionContainer is blank from the line above. Dynamic Layout is controlled in code contained in our CSS file which will affect the classes in our template, so we can ensure everything will be spaced correctly. */
-        collectionContainer.innerHTML += collectionItemTemplate; 
+        var $newThumbnail = buildCollectionItemTemplate();
+        $collectionContainer.append($newThumbnail);
     }
-}
+});
